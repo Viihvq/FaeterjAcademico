d2de587db4a9e4d0fcf7317c990178fad91b1fc0
Viihvq <vitvquintao@gmail.com>
2022-06-06 00:48:10 -0300

    cards style

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Header/>
         <Routes>
-          <Route path="posts" element={(<div><h2>Hello World!</h2></div>)}></Route>
+          <Route path="posts" element={(<div style={{minHeight: "50vh"}}><h2>Página reservada para posts da faculdade.</h2></div>)}></Route>
           <Route path="/" element={<Home/>}></Route>
           <Route path="academico" element={<Login/>}></Route>
           <Route path="academico/infos" element={<Infos/>}></Route>
--- src/components/Infos/index.tsx
+++ src/components/Infos/index.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import style from './Infos.module.scss';
 
 export function Infos(){
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [editTitle, setEditTitle] = useState<string>('');
+  const [editData, setEditData] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [data, setData] = useState<string>('');
+
+  useEffect(() => {
+    setTitle(editTitle);
+  },[editTitle])
+
+  useEffect(() => {
+    setData(editData);
+  },[editData])
+
+  function ableEdit(){
+    setIsDisabled(false);
+  }
+  
+  function saveInfos(){
+    setIsDisabled(true);
+  }
+
   return(
     <>
     <div className={style.container}>
@@ -14,40 +37,27 @@ export function Infos(){
         <button>Editar anotações</button>
       </div>
       <div className={style.content}>
-        <span className={style.titulo}>SISTEMAS OPERACIONAIS 2</span>
-        <span className={style.data}>13/06/2022</span>
+        <input 
+          type="text" 
+          disabled={isDisabled} 
+          className={isDisabled ? style.desativado : style.ativado}
+          onChange={event => setEditTitle(event.target.value)}
+          value={"SISTEMAS OPERACIONAIS 2"}
+          // value={title}
+          />
+        <input 
+          type="text" 
+          disabled={isDisabled} 
+          className={isDisabled ? style.desativado : style.ativado}
+          onChange={event => setEditData(event.target.value)}
+          value={"13/06/2022"}
+          // value={data}
+          />
         <span >ANOTAÇÕES</span>
-        <div className={style.anotacoes}>
-          <span>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA          </span>
+        <div className={style.anotacoes}>          
         </div>
-        <button>Editar anotações</button>
-      </div>
-      <div className={style.content}>
-        <span className={style.titulo}>SISTEMAS OPERACIONAIS 2</span>
-        <span className={style.data}>13/06/2022</span>
-        <span >ANOTAÇÕES</span>
-        <div className={style.anotacoes}>
-          <span>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA          </span>
-        </div>
-        <button>Editar anotações</button>
-      </div>
-      <div className={style.content}>
-        <span className={style.titulo}>SISTEMAS OPERACIONAIS 2</span>
-        <span className={style.data}>13/06/2022</span>
-        <span >ANOTAÇÕES</span>
-        <div className={style.anotacoes}>
-          <span>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA          </span>
-        </div>
-        <button>Editar anotações</button>
-      </div>
-      <div className={style.content}>
-        <span className={style.titulo}>SISTEMAS OPERACIONAIS 2</span>
-        <span className={style.data}>13/06/2022</span>
-        <span >ANOTAÇÕES</span>
-        <div className={style.anotacoes}>
-          <span>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA          </span>
-        </div>
-        <button>Editar anotações</button>
+        <button onClick={ableEdit}>Editar anotações</button>
+        <button onClick={saveInfos}>Salvar alterações</button>
       </div>
     </div>
     </>
