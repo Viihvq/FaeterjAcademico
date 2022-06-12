07059ed2d30f39ac985df9d504bb9f31ac03b55e
Lucas Esteves <lucasesteves5304@gmail.com>
2022-06-12 16:36:44 -0300

    Auth

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -31,11 +31,13 @@ export interface IUser{
 }
 
 interface login{
-  matricula: number | undefined,
+  matricula: string | undefined,
   senha: string | undefined,
+  
 }
 
 export function Login(){
+  const history = useNavigate();
   const [user, setUser] = useState<IUser>();
   const [login, setLogin] = useState<login>();
   let approve = false;
@@ -47,6 +49,9 @@ export function Login(){
     )
   }
 
+  //Comentei pq nao sei como implementar dentro desse carinha 
+
+  /*
   function getUser(id: login | undefined){
     userService.getUserById(id?.matricula)
     .then((response) => {
@@ -63,6 +68,44 @@ export function Login(){
       console.error("Erro! "+error);
     });    
   }
+*/
+  
+
+//MONTANDO SCHEMA DO JSON 
+  var data = {
+    email: login?.matricula,
+    senha: login?.senha
+  };
+
+  //Dispara uma requisição e verifica se o usuario existe, caso exista retorna o token (caso nao exista ele lança uma excepetiob)
+  async function auth() {
+    console.log("fui clicado");
+    try {
+      var { headers}  = await api.post(`/login`, data);
+      //Pegando token Gerado pela requisição
+      localStorage.setItem("token", headers.authorization);
+     //Pegando dados do usuario que se autenticou
+      getUsuario()
+
+    } catch {
+      alert("Aluno invalido")
+    }
+
+  }
+
+  //Get by email, para retornar os dados do usuario que acabou de se autenticar e salvar o usuario no localStorage
+  async function getUsuario() {
+    try {
+      var { data } = await api.get(`/aluno/email?value=${login?.matricula}`);
+      alert(`Bem-Vindo ${data.nome}`) 
+      //Salvando dados do usuario no localStorage (se quiser futuramente acessar o dado estara la, ou pode criar uma Context para persistir)
+      localStorage.setItem("usuario",JSON.stringify( data));
+      //Redicionar para a rota apos o login
+      history("/");
+    } catch {
+      alert("Erro ao se conectar com o servidor (heroku iniciando)")
+    }
+  }
 
   useEffect(() => {
     
@@ -77,11 +120,10 @@ export function Login(){
         <div className={style.imagem}>
           <img src={dragao} alt="" />
         </div>
-        <span>Matrícula</span>
+        <span>Email</span>
         <input 
-          type="number"
           placeholder="Número da matrícula"
-          onChange={event => setLogin({matricula: Number(event.target.value), senha: login?.senha})}
+          onChange={event => setLogin({matricula: event.target.value, senha: login?.senha})}
         />
         <span>Senha</span>
         <input 
@@ -89,7 +131,7 @@ export function Login(){
           onChange={event => setLogin({senha: event.target.value, matricula: login?.matricula})}
         />
         <button
-          onClick={() => getUser(login)}
+          onClick={() => auth()}
         >
           Entrar
         </button>
